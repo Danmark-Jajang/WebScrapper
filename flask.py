f2d6aa8bf8f8d976a16a8fa31e90c2f0a033f62a
Danmark-Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2024-02-24 01:22:33 +0900

    스크랩 결과

---
+++ flask.py
@@ -0,0 +1,10 @@
+import csv
+def save_to_file(file_name, jobs):
+    file = open(f"{file_name}.csv","w")
+    w = csv.writer(file)
+    w.writerow(["title","company","lang","link"])
+
+    for job in jobs:
+        w.writerow(job.values())
+
+    file.close()
